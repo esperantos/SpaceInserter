fc9d094c8f88b5a87ba0ba2d3a8d6e5776356670
«freyberg» <freyberg@skbkontur.ru>
2013-03-01 17:08:42 +0600

    added project to git

---
+++ FileUpdater.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Xml;
+
+namespace SpaceInserter
+{
+	public class FileUpdater
+	{
+		private readonly char[] _digits = new[] {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+
+		public void UpdateFile(string source, string toDirectory)
+		{
+			XmlDocument xmlDocument = ReadXml(source);
+			int updated = Process(xmlDocument);
+			if (updated != 2)
+				Console.Write("!!!  ");
+			Console.WriteLine(string.Format("Обработан файл {0}. Переписано {1} Номеров.", source, updated));
+// ReSharper disable AssignNullToNotNullAttribute
+			WriteXml(Path.Combine(toDirectory, Path.GetFileName(source)), xmlDocument);
+// ReSharper restore AssignNullToNotNullAttribute
+		}
+
+		private int Process(XmlNode xmlNode)
+		{
+			XmlAttribute attr = FindAttribute(xmlNode, "СерНомДок");
+			return UpdateValue(attr) + xmlNode.ChildNodes.Cast<XmlNode>().Sum(child => Process(child));
+		}
+
+		private int UpdateValue(XmlAttribute attr1)
+		{
+			if (attr1 != null && attr1.Value.Length == 10 && attr1.Value.All(c => _digits.Contains(c)))
+			{
+				attr1.Value = attr1.Value.Insert(4, " ");
+				attr1.Value = attr1.Value.Insert(2, " ");
+				return 1;
+			}
+			return 0;
+		}
+
+		private static XmlAttribute FindAttribute(XmlNode xmlNode, string attrName)
+		{
+			XmlAttribute firstOrDefault = null;
+			if (xmlNode != null && xmlNode.Attributes != null)
+			{
+				firstOrDefault = xmlNode.Attributes.Cast<XmlAttribute>().FirstOrDefault(a => a.Name == attrName);
+			}
+			return firstOrDefault;
+		}
+
+
+		private static XmlDocument ReadXml(string path)
+		{
+			XmlDocument xmlDocument = new XmlDocument();
+			using (var fileStream = new FileStream(path, FileMode.Open, FileAccess.Read))
+			{
+				fileStream.Seek(0, SeekOrigin.Begin);
+				xmlDocument.Load(fileStream);
+			}
+			return xmlDocument;
+		}
+
+		private static void WriteXml(string path, XmlDocument xmlDocument)
+		{
+			using (var fileStream = new FileStream(path, FileMode.CreateNew, FileAccess.Write))
+			{
+				XmlWriterSettings settings = new XmlWriterSettings
+					{Indent = true, OmitXmlDeclaration = false, NewLineOnAttributes = false};
+				using (XmlWriter xmlWriter = XmlWriter.Create(fileStream, settings))
+				{
+					xmlDocument.WriteTo(xmlWriter);
+				}
+			}
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+
+namespace SpaceInserter
+{
+	internal class Program
+	{
+		private static int Main(string[] args)
+		{
+			if (args.Length != 2)
+			{
+				Console.WriteLine(
+					"неверное использование! Надо так:\nSpaceInserter <путь к папке с исходными файлами> <папка куда положить модифицированный вариант>\n или\n\nSpaceInserter <путь к файлу> <папка куда положить модифицированный вариант>");
+				return 500;
+			}
+
+			string sourcePath = args[0];
+			string destinationPath = args[1];
+
+			if (!Directory.Exists(destinationPath))
+				Directory.CreateDirectory(destinationPath);
+			var fileUpdater = new FileUpdater();
+			if (File.Exists(sourcePath))
+			{
+				fileUpdater.UpdateFile(sourcePath, destinationPath);
+				return 0;
+			}
+
+			if (Directory.Exists(sourcePath))
+			{
+				string[] files = Directory.GetFiles(sourcePath, "*.xml");
+				if (files.Length == 0)
+					Console.WriteLine(string.Format("В папке \"{0}\" не найдено доверенностей", sourcePath));
+				foreach (string file in files)
+				{
+					try
+					{
+						fileUpdater.UpdateFile(file, destinationPath);
+					}
+					catch (Exception e)
+					{
+						Console.WriteLine(string.Format("Ошибка при обработки файла {0}.\n {1}\n", file, e));
+					}
+				}
+				return 0;
+			}
+			Console.WriteLine(string.Format("Папка или файл {0} не найден", sourcePath));
+			return 404;
+		}
+	}
+}
